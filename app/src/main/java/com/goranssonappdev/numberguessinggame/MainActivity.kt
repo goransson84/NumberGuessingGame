f0377d2958c7820ceb6edaf7f8d21bbfd5dbae6e
goransson84 <goransson84@gmail.com>
2022-04-15 14:54:11 +0200

    LazyListNumber

--- app/src/main/java/com/goranssonappdev/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/goranssonappdev/numberguessinggame/MainActivity.kt
@@ -4,27 +4,40 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.goranssonappdev.numberguessinggame.ui.theme.NumberGuessingGameTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
 
             var randomNumber = rememberSaveable {
                 mutableStateOf(randomNumber())
             }
+//            var guestNumbers = rememberSaveable {
+//                mutableStateListOf()
+//            }
 
             NumberGuessingGameTheme {
                 Scaffold() {
@@ -39,15 +52,35 @@ class MainActivity : ComponentActivity() {
                         }
                         ) {
                             Text(text = "Random Number")
-
                         }
-                        
+                        LazyListNumber()
+
 
                     }
 
                 }
-
             }
+        }
+    }
+}
+
+
+@Composable
+fun LazyListNumber() {
+    LazyColumn {
+        items(100) {
+            Text(text = "$it",
+                fontSize = 28.sp,
+                modifier = Modifier
+                    .clip(RoundedCornerShape(12.dp))
+                    .background(color = Color.Green)
+                    .clickable(
+                        enabled = true,
+                        role = Role.Button
+                    ) {
+                        Log.d("Tag", "$it")
+                    }
+            )
         }
     }
 }
