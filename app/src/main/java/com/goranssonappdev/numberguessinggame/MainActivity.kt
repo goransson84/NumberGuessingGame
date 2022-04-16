d039fcfa752f387ea6b7681a466a4336a2a450cc
goransson84 <goransson84@gmail.com>
2022-04-16 07:48:16 +0200

    LazyList Card

--- app/src/main/java/com/goranssonappdev/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/goranssonappdev/numberguessinggame/MainActivity.kt
@@ -6,21 +6,17 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.goranssonappdev.numberguessinggame.ui.theme.NumberGuessingGameTheme
@@ -32,55 +28,63 @@ class MainActivity : ComponentActivity() {
 
         setContent {
 
-            var randomNumber = rememberSaveable {
-                mutableStateOf(randomNumber())
+            val guestNumbers = rememberSaveable {
+                mutableStateOf(mutableSetOf<Int>(10,11,2,22))
             }
-//            var guestNumbers = rememberSaveable {
-//                mutableStateListOf()
-//            }
 
             NumberGuessingGameTheme {
-                Scaffold() {
-                    Column(modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Center,
-                        horizontalAlignment = Alignment.CenterHorizontally
-                    ) {
-                        Text(text = randomNumber.value.toString(), fontSize = 50.sp)
-                        Button(onClick = {
-                            randomNumber.value = randomNumber()
-                            Log.d("Tag", randomNumber.toString())
-                        }
-                        ) {
-                            Text(text = "Random Number")
-                        }
-                        LazyListNumber()
 
+                Scaffold(
+                    topBar = {
+                        TopAppBar {
 
+                        }
                     }
+                ) {
+                    Row(modifier = Modifier.fillMaxSize()
+                    ) {
 
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .weight(4f)
+                                .background(color = Color.LightGray)
+                        ) {
+                            Text(
+                                text = "1F",
+                                style = MaterialTheme.typography.caption
+                            )
+                        }
+
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .weight(1f)
+                                .background(color = Color.LightGray)
+                        ) {
+                            LazyColumn {
+                                items(1000 + 1) {
+                                    Card(modifier = Modifier
+                                        .fillMaxSize()
+                                        .padding(5.dp)
+                                        //.background(if ("$it".toInt() !in guestNumbers.value) Color.Green else Color.Red)
+                                        .clickable {
+                                            guestNumbers.value.add("$it".toInt())
+                                            Log.d("Tag", "${guestNumbers.value}")
+                                        },
+                                        elevation = 10.dp,
+                                        backgroundColor = if ("$it".toInt() in guestNumbers.value) Color.Red else Color.LightGray
+                                    ) {
+                                        Text(text = "$it", fontSize = 28.sp, textAlign = TextAlign.Center)
+
+                                    }
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-
-@Composable
-fun LazyListNumber() {
-    LazyColumn {
-        items(100) {
-            Text(text = "$it",
-                fontSize = 28.sp,
-                modifier = Modifier
-                    .clip(RoundedCornerShape(12.dp))
-                    .background(color = Color.Green)
-                    .clickable(
-                        enabled = true,
-                        role = Role.Button
-                    ) {
-                        Log.d("Tag", "$it")
-                    }
-            )
-        }
-    }
-}
