adf489f2f9282bf6b8d9d7faf63a2fe3b7dbf08c
goransson84 <goransson84@gmail.com>
2022-04-19 20:30:06 +0200

    Rebuild

--- app/src/main/java/com/goranssonappdev/numberguessinggame/MainActivity.kt
+++ app/src/main/java/com/goranssonappdev/numberguessinggame/MainActivity.kt
@@ -6,14 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.*
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -29,8 +27,21 @@ class MainActivity : ComponentActivity() {
         setContent {
 
             val guestNumbers = rememberSaveable {
-                mutableStateOf(mutableSetOf<Int>(10,11,2,22))
+                mutableStateOf(setOf<Int>())
             }
+
+            val rightNumber = rememberSaveable {
+                mutableStateOf(randomNumber())
+            }
+
+            val numberOfGuesses = rememberSaveable {
+                mutableStateOf("3")
+            }
+
+            val text = rememberSaveable {
+                mutableStateOf("3")
+            }
+
 
             NumberGuessingGameTheme {
 
@@ -50,10 +61,12 @@ class MainActivity : ComponentActivity() {
                                 .weight(4f)
                                 .background(color = Color.LightGray)
                         ) {
-                            Text(
-                                text = "1F",
-                                style = MaterialTheme.typography.caption
-                            )
+                            Column(modifier = Modifier.fillMaxSize(),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(text = "${rightNumber.value}", fontSize = 28.sp)
+                            }
                         }
 
                         Box(
@@ -63,19 +76,35 @@ class MainActivity : ComponentActivity() {
                                 .background(color = Color.LightGray)
                         ) {
                             LazyColumn {
-                                items(1000 + 1) {
+                                items(100 + 1) {
                                     Card(modifier = Modifier
                                         .fillMaxSize()
                                         .padding(5.dp)
-                                        //.background(if ("$it".toInt() !in guestNumbers.value) Color.Green else Color.Red)
                                         .clickable {
-                                            guestNumbers.value.add("$it".toInt())
-                                            Log.d("Tag", "${guestNumbers.value}")
+
+
+                                            if (numberOfGuesses.value.toInt() == guestNumbers.value.size) {
+                                                Log.d("Tag",
+                                                    "${numberOfGuesses.value.toInt()} == ${guestNumbers.value.size}")
+                                            } else {
+                                                val guess = guestNumbers.value.toMutableSet()
+                                                guess.add("$it".toInt())
+                                                guestNumbers.value = guess.toSet()
+
+                                                Log.d("Tag", "${guestNumbers.value}")
+                                                if ("$it".toInt() == rightNumber.value) {
+                                                    Log.d("Tag", "Right number guessed")
+                                                }
+                                            }
+
+
                                         },
                                         elevation = 10.dp,
                                         backgroundColor = if ("$it".toInt() in guestNumbers.value) Color.Red else Color.LightGray
                                     ) {
-                                        Text(text = "$it", fontSize = 28.sp, textAlign = TextAlign.Center)
+                                        Text(text = "$it",
+                                            fontSize = 28.sp,
+                                            textAlign = TextAlign.Center)
 
                                     }
                                 }
@@ -87,4 +116,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+
+
 
